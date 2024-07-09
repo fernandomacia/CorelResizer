7603d87f94b62b13673b93924557ed6b3535d291
Fernando Maciá <fernando.macia@hotmail.es>
2024-07-09 16:55:18 +0200

    Version 1.04

--- CorelResizer.vb
+++ CorelResizer.vb
@@ -1,8 +1,8 @@
-Sub SS06()
+Sub CorelResizer()
 
     'Macro que redimensiona círculos a una medida dada por teclado
     '@autor: Fernando Maciá
-    '@version: 1.03 27/10/21
+    '@version: 1.04 09/07/2024
     
     Dim sr As ShapeRange, nr As ShapeRange, s As Shape, size, Pregunta
     
@@ -14,7 +14,7 @@ Sub SS06()
         MsgBox "No hay documentos abiertos.", vbOKOnly + vbCritical
         Exit Sub
     Else
-        If sr.Count < 1 Then
+        If sr.Count < 1 Then 'En caso de no haber elementos seleccionados termina el proceso
             MsgBox "No hay objetos seleccionados.", vbOKOnly + vbCritical
             Exit Sub
         End If
@@ -23,39 +23,35 @@ Sub SS06()
     ActiveDocument.Unit = cdrMillimeter 'Indicamos que la unidad de medida son milimetros
     ActiveDocument.ReferencePoint = cdrCenter 'Indicamos que el punto de referencia es el centro de los objetos
     
-    If sr.Count >= 1 Then 'Si hay objetos seleccionados
-            
-        size = InputBox("Ingrese el tamaño")
-        
-        If size = "" Then
-            Exit Sub
-        Else
-
-         If IsNumeric(size) Then
-             sr.UngroupAll 'Desagrupamos los objetos
-             For Each s In sr
-                 nr.AddRange s.BreakApartEx
-                 Next s
-                 nr.CreateSelection
-         
-             For Each s In nr
-                 s.SetSize size, size
-                 Next s
-         
-             If nr.Count > 1 Then 'Si los objetos seleccionados son mas de uno
-                 Pregunta = MsgBox("Quieres combinar los objetos?", vbYesNo + vbQuestion)
-        
-                 If Pregunta = vbYes Then
-                     Set s = nr.Combine
-                     s.CreateSelection
-                 End If
-             End If
-         Else
-             MsgBox "El dato introducido no tiene un formato correcto.", vbOKOnly + vbCritical
-             Exit Sub
-         End If
-        End If
+    size = InputBox("Ingrese el tamaño")
+    
+    If size = "" Then
+        MsgBox "No se ha seleccionado el tamaño.", vbOKOnly + vbCritical
+        Exit Sub
     Else
-        MsgBox "No hay objetos seleccionados.", vbOKOnly + vbExclamation
+        If IsNumeric(size) Then
+            sr.UngroupAll 'Desagrupamos los objetos
+            For Each s In sr
+                nr.AddRange s.BreakApartEx
+                Next s
+                nr.CreateSelection
+        
+            For Each s In nr
+                s.SetSize size, size
+                Next s
+        
+            If nr.Count > 1 Then 'Si los objetos seleccionados son mas de uno
+                Pregunta = MsgBox("Quieres combinar los objetos?", vbYesNo + vbQuestion)
+    
+                If Pregunta = vbYes Then
+                    Set s = nr.Combine
+                    s.CreateSelection
+                End If
+            End If
+        Else
+            MsgBox "El dato introducido no tiene un formato correcto.", vbOKOnly + vbCritical
+            Exit Sub
+        End If
     End If
+
 End Sub
--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # CorelResizer
-Macro que redimensiona objetos de Corel Draw
+
+## Introducción
+
+Este proyecto tiene como objetivo solucionar una problemática de Corel Draw acerca de cómo redimensiona un conjunto de objetos.
+
+## Aplicación :gem:
+
+<img src="static/img/strass.jpg" alt="Strass" width="250" style="float: left; margin-right: 20px;"/>Al realizar diseños para strass se utilizan programas de dibujo vectorizado. En el programa Corel Draw se ha detectado un problema con el redimensionado de múltiples objetos, y este problema es el siguiente: 
+<img src="static/img/imagen1.jpg" alt="Composición ss10" width="240" style="float: right; margin-left: 10px;"/>
+Si tenemos dibujados varios objetos circulares de un tamaño ss10 (3mm) y queremos cambiarlos por otros de tamaño ss06 (2mm), debemos de ir seleccionando cada uno de los objetos y redimensionarlo uno por uno, ya que no existe herramienta que tras hacer la selección de todos estos objetos cambie el tamaño de cada uno de los objetos manteniendo su posición. 
+<img src="static/img/zapato.jpeg" alt="Zapato con strass" width="150" style="float: left; margin-right: 20px; margin-top: 20px;"/>
+Este tipo de diseños es muy usual en sectores como el del calzado, confección, bolsos, etc... Como podremos imaginar la variedad de diseños es muy amplia, habiendo unos muy sencillos con una cantidad pequeña de cristales y otros que utilizan más de 8000. Cuando queremos hacer pruebas para ver que efecto nos hace cambiar el tamaño de los cristales en estos diseños tan complejos es cuando el trabajo que supone redimensionarlos uno por uno es muy tedioso. Por este motivo me decidí a relizar un macro que automatizara esta función, disminuyendo significativamente el tiempo en realizar el trabajo, así como la eliminación de errores por la manipulación de cada uno de los objetos.
+
+### Capturas de Pantalla
+
+#### Formulario selección tamaño
+<img src="static/img/captura1.jpg" alt="Strass" width="500" style="display: block;"/>
+
+#### Formulario combinación
+<img src="static/img/captura2.jpg" alt="Strass" width="500" style="display: block;"/>
+
+## Tecnologías Utilizadas :hammer_and_wrench:
+
+- **Visual Basic**: Lenguaje de programación utilizado para el desarrollo del macro para la aplicación.
+- **Corel Draw**: Programa de diseño vectorizado donde vamos a utilizar el macro programado.
+
+## Versiones :pushpin:
+
+Esta macro ha sido probada en corelDraw X6, X8,
+
+## Licencia :page_with_curl:
+
+Este proyecto está bajo la Licencia MIT.
+
+## Autor
+
+- [Fernando Maciá](https://github.com/fernandomacia)
+
